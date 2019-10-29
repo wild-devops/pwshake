35c4cd5672decb33a78af5ed8d870c9bd0c730ad
wild-devops <33394863+wild-devops@users.noreply.github.com>
2019-10-29 12:29:57 +0300

    Adjust for GitHub Actions

--- pwshake.ps1
+++ pwshake.ps1
@@ -30,6 +30,7 @@ $ErrorActionPreference = "Stop"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
 
 if ([System.Environment]::OSVersion.Platform -match 'Win') {
+  Import-Module -Name PackageManagement -Force -Global -DisableNameChecking
   if (-not (Get-PackageProvider -ListAvailable | Where-Object Name -eq NuGet)) {
     Set-PackageSource -Name PSGallery -Trusted -Force | Out-Null
     Install-PackageProvider NuGet -Force | Out-Null
